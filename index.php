39e72e5caef32a2b7d65a46d0b89f533934f881c
Roland Golla <rolandgolla@gmail.com>
2018-07-20 21:07:58 +0200

    Add scripts to index file

--- index.php
+++ index.php
@@ -1,40 +1,56 @@
 <!doctype html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-    <title>Talks Never Code Alone</title>
-    <meta name="description" content="Sammlung von Talks über Projekte, Kunden und Web Development Technologien"/>
+        <title>Talks Never Code Alone</title>
+        <meta name="description" content="Sammlung von Talks über Projekte, Kunden und Web Development Technologien"/>
 
-    <link rel="stylesheet" href="css/reveal.css">
-    <link rel="stylesheet" href="css/theme/black.css">
-    <link rel="stylesheet" href="nca/css/nca.css">
+        <link rel="stylesheet" href="css/reveal.css">
+        <link rel="stylesheet" href="css/theme/black.css">
+        <link rel="stylesheet" href="nca/css/nca.css">
 
-    <!-- Theme used for syntax highlighting of code -->
-    <link rel="stylesheet" href="lib/css/zenburn.css">∆
+        <!-- Theme used for syntax highlighting of code -->
+        <link rel="stylesheet" href="lib/css/zenburn.css">∆
 
-    <!-- Printing and PDF exports -->
-    <script>
-        var link = document.createElement( 'link' );
-        link.rel = 'stylesheet';
-        link.type = 'text/css';
-        link.href = window.location.search.match( /print-pdf/gi ) ? 'css/print/pdf.css' : 'css/print/paper.css';
-        document.getElementsByTagName( 'head' )[0].appendChild( link );
-    </script>
-    <link rel="shortcut icon" href="nca/img/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="nca/img/favicon.ico" type="image/x-icon">
-</head>
-<body>
-    <a href="https://nevercodealone.de"><img class="logo" width="80" src="nca/img/logo.png"></a>
-    <div class="reveal">
-        <div class="slides">
-            <section>
-                <h1>Talks - Never Code Alone</h1>
-                <p>
-                    <a href="emma-open-source.html">EMMA - Übersetzungstool</a>
-                </p>
-            </section>
-        </div>
-    </div>
-</body>
+        <!-- Printing and PDF exports -->
+        <script>
+            var link = document.createElement( 'link' );
+            link.rel = 'stylesheet';
+            link.type = 'text/css';
+            link.href = window.location.search.match( /print-pdf/gi ) ? 'css/print/pdf.css' : 'css/print/paper.css';
+            document.getElementsByTagName( 'head' )[0].appendChild( link );
+        </script>
+        <link rel="shortcut icon" href="nca/img/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="nca/img/favicon.ico" type="image/x-icon">
+    </head>
+    <body>
+        <a href="https://nevercodealone.de"><img class="logo" width="80" src="nca/img/logo.png"></a>
+        <div class="reveal">
+            <div class="slides">
+                <section>
+                    <h1>Talks - Never Code Alone</h1>
+                    <p>
+                        <a href="emma-open-source.html">EMMA - Übersetzungstool</a>
+                    </p>
+                </section>
+            </div>
+            <script src="lib/js/head.min.js"></script>
+            <script src="js/reveal.js"></script>
+
+            <script>
+                // More info about config & dependencies:
+                // - https://github.com/hakimel/reveal.js#configuration
+                // - https://github.com/hakimel/reveal.js#dependencies
+                Reveal.initialize({
+                    dependencies: [
+                        { src: 'plugin/markdown/marked.js' },
+                        { src: 'plugin/markdown/markdown.js' },
+                        { src: 'plugin/notes/notes.js', async: true },
+                        { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
+                    ]
+                });
+            </script>
+    </body>
+</html>
